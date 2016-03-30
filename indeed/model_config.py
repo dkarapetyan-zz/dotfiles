8a2cd4da8a84102dadd2e8e62d9ae7f95a5d2240
David Karapetyan <david.karapetyan@gmail.com>
2016-03-30 09:16:57 -0400

    indeed exam

---
+++ indeed/model_config.py
@@ -0,0 +1,13 @@
+
+model_config = dict(
+        rfr={
+            'params': {
+                'n_estimators': 40,
+                'criterion': 'mse',
+                'max_features': None,  # max_features=n_features
+                'n_jobs': -1,
+                'oob_score': True,  # use out of bag samples to estimate error
+                'verbose': False
+            }
+        }
+)
