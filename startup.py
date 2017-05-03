9f7ce1a18b71a176930947f0f4a1a424448adb2c
David Karapetyan <david.karapetyan@gmail.com>
2017-05-02 23:40:19 -0400

    installed dotbot. Sane management of dotfiles

--- startup.py
+++ startup.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib
-matplotlib.use('Qt5Agg')
+# import matplotlib
+# matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 pd.set_option('display.width', pd.util.terminal.get_terminal_size()[0])
